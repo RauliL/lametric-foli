45aff9f6b91aaffcd37c62a5dc85a05de4bac154
Rauli Laine <rauli.laine@iki.fi>
2017-11-17 23:24:07 +0200

    Rewrite the damn thing
    hook.io has axios and moment.js libraries installed, so utilize them and
    introduce much nicer version of the backend, which now displays minutes
    until a bus arrives at a stop.

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-const http = require('http');
+const axios = require('axios');
+const moment = require('moment');
 
-const sendError = (hook, message = 'Unable to connect Föli API') => {
-  hook.res.json({
-    frames: [
-      {
-        text: message,
-        icon: 'stop'
-      }
-    ]
-  });
-};
-
-const formatTime = time => {
-  const prefixWithZero = value => `${value < 10 ? '0' : ''}${value}`;
-  const hours = time.getHours();
-  const minutes = time.getMinutes();
-  
-  return `${prefixWithZero(hours)}:${prefixWithZero(minutes)}`;
-};
-
+const sendError = (hook, message) => hook.res.json({
+  frames: [
+    {
+      text: message,
+      icon: 'stop'
+    }
+  ]
+});
 
 module.exports = hook => {
   // Specify response content type and character set.
@@ -29,55 +19,35 @@ module.exports = hook => {
     sendError(hook, 'Missing bus stop ID');
     return;
   }
-  
+
   // Attempt to retrieve arrival data from the Föli API.
-  http.get(
-    `http://data.foli.fi/siri/sm/${hook.params.id}`,
-    response => {
-      const { statusCode } = response;
-      const contentType = response.headers['content-type'];
-      let rawData = '';
+  axios.get(`http://data.foli.fi/siri/sm/${hook.params.id}`)
+    .then(response => {
+      const now = moment();
+      const arrivals = response.data.result.filter(arrival => arrival.expectedarrivaltime >= now.unix());
 
-      // Check whether the API returned HTTP response 200 and some JSON.
-      if (statusCode !== 200 || !/^(application|text)\/json/.test(contentType)) {
-        sendError(hook);
-        return;
+      if (arrivals.length > 0) {
+        hook.res.json({
+          frames: arrivals.slice(0, 5).map(arrival => {
+            const time = moment.unix(arrival.expectedarrivaltime);
+            const line = arrival.lineref;
+            const display = arrival.destinationdisplay;
+            const difference = time.diff(now, 'minutes');
+
+            return {
+              text: `${difference} min - ${line} / ${display}`,
+              icon: 'föli'
+            };
+          })
+        });
+      } else {
+        hook.res.json({
+          frames: [{
+            text: 'No arrivals',
+            icon: 'föli'
+          }]
+        });
       }
-
-      // Read the entire response into local variable.
-      response.setEncoding('utf8');
-      response.on('data', chunk => { rawData += chunk; });
-      response.on('end', () => {
-        try {
-          const parsedData = JSON.parse(rawData);
-          const now = Date.now();
-          const futureArrivals = parsedData.result.filter(arrival => arrival.expectedarrivaltime > now);
-          
-          if (futureArrivals.length > 0) {
-            hook.res.json({
-              frames: futureArrivals.result.slice(0, 5).map(arrival => {
-                const time = new Date(arrival.expectedarrivaltime * 1000);
-                const line = arrival.lineref;
-                const display = arrival.destinationdisplay;
-                
-                return {
-                  text: `${formatTime(time)} - ${line} - ${display}`,
-                  icon: 'föli'
-                };
-              })
-            });
-          } else {
-            hook.res.json({
-              frames: [{
-                text: 'No arrivals',
-                icon: 'föli'
-              }]
-            });
-          }
-        } catch (e) {
-          sendError(hook);
-        }
-      });
-    }
-  ).on('error', () => sendError(hook));
+    })
+    .catch(() => sendError(hook, 'Unable to connect to Föli API'));
 };
